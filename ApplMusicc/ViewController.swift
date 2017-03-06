5321dcc61541f473148ed9a033cb9eb5cfe6969b
Juan Andres Garcia C <JAGC@iMac-de-Juan.local>
2017-03-06 16:48:16 -0500

    Initial Commit

---
+++ ApplMusicc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ApplMusicc
+//
+//  Created by Juan Andres Garcia C on 6/03/17.
+//  Copyright © 2017 Juan Andres Garcia C. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
